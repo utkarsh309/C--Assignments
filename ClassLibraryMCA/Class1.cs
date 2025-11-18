6c02201119db438996b3e5a3f7529bad4174ebc0
utkarsh <utkarshraj309@gmail.com>
2025-11-18 14:44:41 +0530

    Class Library Program

---
+++ ClassLibraryMCA/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ClassLibraryMCA
+{
+    public class StringUtils
+    {
+        public static string SpaceRemover(string input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+            return input.TrimStart().TrimEnd();
+        }
+        public static string StringReverser(string input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
